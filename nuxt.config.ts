83cd30ae78c4b8109c505bd20612fa8a6be7d07d
Chris Hatton <christopher.hatton@transreport.co.uk>
2023-01-04 17:26:21 +0000

    feat(package): eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    // https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
 })
