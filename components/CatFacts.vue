edbca858cd3458dc7ce51d832d31ff858bed8646
Chris Hatton <christopher.hatton@transreport.co.uk>
2023-01-09 17:21:00 +0000

    fix(component): catfacts v-for key

--- components/CatFacts.vue
+++ components/CatFacts.vue
@@ -3,7 +3,7 @@
     <h2 class="text-2xl">Cat Facts 🐈</h2>
     <p class="my-4">This grabs cat facts from an API (<a href="https://catfact.ninja/#/Facts/getFacts" target="_blank">https://catfact.ninja/#/Facts/getFacts</a>) via fetch.</p>
     <section class="flex flex-wrap">
-      <div v-for="catFact in facts" class="w-full sm:w-1/2 md:w-1/3 xl:w-1/4" :key="catFact.fact.split(' ')[0]">
+      <div v-for="(catFact, index) in facts" class="w-full sm:w-1/2 md:w-1/3 xl:w-1/4" :key="catFact.fact.split(' ')[0] + index">
         <div class="bg-white rounded-lg shadow-lg m-4 p-4">
           <h3>{{ catFact.fact }}</h3>
         </div>
@@ -16,7 +16,7 @@ export default {
   name: 'CatFacts',
   data () {
     return {
-      facts: {}
+      facts: []
     }
   },
   
