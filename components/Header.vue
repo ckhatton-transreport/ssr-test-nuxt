a9cc7c33ad89f489f5fb34e0c154e3eae0511d31
Chris Hatton <christopher.hatton@transreport.co.uk>
2023-01-23 15:03:15 +0000

    build(component): rename from common markup

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="bg-aquamarine mb-4 p-8 rounded-lg shadow-lg font-extrabold text-white tracking-wide flex flex-wrap justify-between items-center">
-    <h2 class="text-2xl">Nuxt 3</h2>
+    <h2 class="text-2xl">
+      Nuxt 3
+    </h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'BaseHeader'
 }
 </script>
